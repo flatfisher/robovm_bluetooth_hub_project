dabc461ecf7cca09d3dcedca4d540e66f48bc23a
Kyohei Tsuda <shimano.entou@gmail.com>
2015-11-27 19:35:31 -0800

    Prepare iOS project files.

--- ios/src/main/java/com/liferay/bluetooth/MainViewController.java
+++ ios/src/main/java/com/liferay/bluetooth/MainViewController.java
@@ -1,9 +1,7 @@
 package com.liferay.bluetooth;
 
-
 import org.robovm.apple.uikit.UIViewController;
 import org.robovm.objc.annotation.CustomClass;
-
 
 @CustomClass("MainViewController")
 public class MainViewController extends UIViewController {
