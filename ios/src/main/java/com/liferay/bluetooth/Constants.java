84e8e76493fe7e35c91525b107212d7c33aefed4
Kyohei Tsuda <shimano.entou@gmail.com>
2015-11-28 13:16:30 -0800

    add CHECKED_DEVICE_KEY.

--- ios/src/main/java/com/liferay/bluetooth/Constants.java
+++ ios/src/main/java/com/liferay/bluetooth/Constants.java
@@ -2,9 +2,13 @@ package com.liferay.bluetooth;
 
 public class Constants {
 
+    private Constants(){}
+
     public static final String ScanResultCell = "ScanResultCell";
 
     public static final String CONFIG_DATA_KEY = "config_data_key";
+
+    public static final String CHECKED_DEVICE_KEY = "checked_device_key";
 
     public static final String SCAN_DEVICE = "Scan Bluetooth.";
 
