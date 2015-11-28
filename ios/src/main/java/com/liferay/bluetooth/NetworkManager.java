7e8dd7df9d0a75bdb42c7e68f4372014ec3ee496
Kyohei Tsuda <shimano.entou@gmail.com>
2015-11-28 10:42:45 -0800

    Add NetworkManager for get configuration.

--- ios/src/main/java/com/liferay/bluetooth/NetworkManager.java
+++ ios/src/main/java/com/liferay/bluetooth/NetworkManager.java
@@ -1,7 +1,45 @@
 package com.liferay.bluetooth;
 
-/**
- * Created by flatfisher on 11/28/15.
- */
+import org.robovm.apple.foundation.*;
+import org.robovm.objc.block.VoidBlock3;
+
 public class NetworkManager {
+
+    private NetworkManager() {
+    }
+
+    public static void getConfiguration(VoidBlock3<NSData, NSURLResponse, NSError> newDataTaskChangeListener) {
+        NSURL requestUrl = new NSURL("https://apiflatfisher.appspot.com/config");
+
+        NSURLRequest request = new NSURLRequest(requestUrl);
+
+        NSURLSession session = NSURLSession.getSharedSession();
+
+        NSURLSessionDataTask dataTask = session.newDataTask(request, newDataTaskChangeListener);
+
+        dataTask.resume();
+    }
+
+    public static void postToServer(String value) {
+
+        NSURL requestUrl = new NSURL("https://apiflatfisher.appspot.com/");
+
+        NSMutableURLRequest nsMutableURLRequest = new NSMutableURLRequest();
+
+        nsMutableURLRequest.setURL(requestUrl);
+
+        nsMutableURLRequest.setHTTPMethod("POST");
+
+        NSString requestBody = new NSString("name=" + value);
+
+        nsMutableURLRequest.setHTTPBody(requestBody.toData(NSStringEncoding.UTF8));
+
+        NSURLSession nsurlSession = NSURLSession.getSharedSession();
+
+        NSURLSessionDataTask nsurlSessionDataTask = nsurlSession.newDataTask(nsMutableURLRequest);
+
+        nsurlSessionDataTask.resume();
+
+    }
+
 }
