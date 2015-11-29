c63db74fa6e6d75ad7fc4d14aa156ac2403d3cfd
Kyohei Tsuda <shimano.entou@gmail.com>
2015-11-28 20:36:51 -0800

    Upgrade convertibility string to byte array.

--- ios/src/main/java/com/liferay/bluetooth/Convert.java
+++ ios/src/main/java/com/liferay/bluetooth/Convert.java
@@ -1,7 +1,31 @@
 package com.liferay.bluetooth;
 
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
 /**
  * Created by flatfisher on 11/27/15.
  */
 public class Convert {
+
+    private Convert(){}
+
+    public static byte[] stringToByteArray(String value){
+        byte[] hex = new byte[0];
+
+        try {
+
+            hex = Hex.decodeHex(value.toCharArray());
+
+            return hex;
+
+        } catch (DecoderException decoderException) {
+
+            System.out.println("decoderException " + decoderException);
+
+        }
+
+        return hex;
+    }
+
 }
