3540321d1c3f49b1b30f804620660df92e97761e
Kyohei Tsuda <shimano.entou@gmail.com>
2015-11-30 20:19:14 -0800

    Add class for save config to mobile device and get config from mobile device.

--- android/src/main/java/com/liferay/bluetooth/DataManager.java
+++ android/src/main/java/com/liferay/bluetooth/DataManager.java
@@ -1,7 +1,34 @@
 package com.liferay.bluetooth;
 
-/**
- * Created by flatfisher on 11/30/15.
- */
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class DataManager {
+
+    private DataManager() {
+
+    }
+
+    public static void saveConfigData(Context context, String jsonString) {
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences(
+                Constants.SHARED_PREFERENCE_NAME_CONFIG,
+                Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(Constants.CONFIG_DATA_KEY, jsonString);
+
+        editor.commit();
+    }
+
+    public String getConfigData(Context context) {
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences(Constants.SHARED_PREFERENCE_NAME_CONFIG,
+                Context.MODE_PRIVATE);
+
+        return sharedPreferences.getString(Constants.CONFIG_DATA_KEY, Constants.NO_CONFIG_MESSAGE);
+
+    }
+
 }
