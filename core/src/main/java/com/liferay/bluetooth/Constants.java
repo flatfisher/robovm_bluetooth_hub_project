53c30bd042ed3c5e5f5c07b7d915983189c540cd
Kyohei Tsuda <shimano.entou@gmail.com>
2015-11-30 20:19:56 -0800

    Add variables.

--- core/src/main/java/com/liferay/bluetooth/Constants.java
+++ core/src/main/java/com/liferay/bluetooth/Constants.java
@@ -4,7 +4,15 @@ public class Constants {
 
     private Constants(){}
 
-    public static final String ScanResultCell = "ScanResultCell";
+    public static final int REQUEST_ENABLE_BLUETOOTH = 1;
+
+    public static final String REQUEST_URL = "https://apiflatfisher.appspot.com";
+
+    public static final String CONFIG_URL = "/config";
+
+    public static final String SCAN_RESULT_CELL = "SCAN_RESULT_CELL";
+
+    public static final String SHARED_PREFERENCE_NAME_CONFIG = "shared_preference_name_config";
 
     public static final String CONFIG_DATA_KEY = "config_data_key";
 
@@ -19,5 +27,7 @@ public class Constants {
     public static final String GATT = "GATT";
 
     public static final String BROADCAST = "Broadcast";
+
+    public static final String UNABLE_RETRIEVE = "Unable to retrieve web page. URL may be invalid.";
 
 }
