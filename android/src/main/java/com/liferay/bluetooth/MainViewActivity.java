a66f2602eb63e05456da555180ad58aca4da3721
Kyohei Tsuda <shimano.entou@gmail.com>
2015-11-30 16:33:28 -0800

    Setup Android project.

---
+++ android/src/main/java/com/liferay/bluetooth/MainViewActivity.java
@@ -0,0 +1,17 @@
+package com.liferay.bluetooth;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_my);
+
+    }
+
+}
