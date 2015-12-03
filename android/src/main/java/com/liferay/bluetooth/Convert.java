a4a552ff143f8e550bfee6ceb5c0d290c7616b62
Kyohei Tsuda <shimano.entou@gmail.com>
2015-12-02 18:37:18 -0800

    Add Convert.

---
+++ android/src/main/java/com/liferay/bluetooth/Convert.java
@@ -0,0 +1,42 @@
+package com.liferay.bluetooth;
+
+public class Convert {
+
+    private Convert(){}
+
+    public static byte[] hexStringToByteArray(String string) {
+
+        int len = string.length();
+
+        byte[] data = new byte[len / 2];
+
+        for (int i = 0; i < len; i += 2) {
+
+            data[i / 2] = (byte) ((Character.digit(string.charAt(i), 16) << 4) + Character
+                    .digit(string.charAt(i + 1), 16));
+
+        }
+
+        return data;
+
+    }
+
+    public static String byteToDecimalString(byte value) {
+
+        String decimalString = String.valueOf(value & 0xff);
+
+        return decimalString;
+
+    }
+
+
+    public static String byteToAscii(byte value) {
+
+        char[] data = new char[1];
+
+        data[0] = (char) value;
+
+        return new String(data);
+
+    }
+}
